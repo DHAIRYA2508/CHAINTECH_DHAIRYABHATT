7f12d1fbae773d2bca8e5c84cc221da922e5c6a1
Dhairya <dhairyabhatt88@gmail.com>
2026-05-02 21:31:04 +0530

    feat: initial simple express server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('Task Manager API is running!');
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
